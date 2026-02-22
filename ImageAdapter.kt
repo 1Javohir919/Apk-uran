8dc2b1c704c8e915d0545662f5a237eeb4009201
1Javohir919 <xolikovnizom@gmail.com>
2026-02-22 23:40:28 +0500

    Create ImageAdapter.kt

---
+++ ImageAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.app.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.app.R
+
+class ImageAdapter(private val imageUrls: List<String>) : RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val imageView: ImageView = view.findViewById(R.id.imageView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_image, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Glide.with(holder.imageView.context)
+            .load(imageUrls[position])
+            .centerCrop()
+            .into(holder.imageView)
+    }
+
+    override fun getItemCount(): Int {
+        return imageUrls.size
+    }
+}
